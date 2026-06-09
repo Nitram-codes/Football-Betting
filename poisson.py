d5107cda50ed6fcd5b546faa10197f70f17b8d6c
Martin <Martin email>
2026-06-09 15:37:50 +0100

    added test for poisson

--- poisson.py
+++ poisson.py
@@ -1,7 +1,9 @@
 import math
 import numpy as np
 
-def poisson_calc(goals, expected_goals):
+def poisson_calc(goals: int, expected_goals: float) -> float: 
+
+    """Computes the Poisson pmf"""
 
     prob = expected_goals**goals * np.exp(-expected_goals) / math.factorial(goals)
 
